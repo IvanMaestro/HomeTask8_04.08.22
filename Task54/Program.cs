3033943e7e90bba53676fd71d4dd24563f223bd9
Masterov_IN <masterov_in@mail.ru>
2022-09-03 23:00:53 +0300

    Task 54 done

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -11,3 +11,54 @@
 // 8 4 4 2
 
 
+int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void ArrangeMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1) - 1; j++)
+        {
+            int max = j;
+            for (int k = j + 1; k < matr.GetLength(1); k++)
+            {
+                if (matr[i, k] > matr[i, max]) max = k;
+            }
+            int temp = matr[i, j];
+            matr[i, j] = matr[i, max];
+            matr[i, max] = temp;
+        }
+    }
+}
+
+int[,] matrix = CreateMatrixRndInt(3, 4, -10, 10);
+PrintMatrix(matrix);
+Console.WriteLine();
+ArrangeMatrix(matrix);
+PrintMatrix(matrix);
