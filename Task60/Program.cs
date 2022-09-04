7fa8b7d835c9819e5f517756c68404069e47672e
Masterov_IN <masterov_in@mail.ru>
2022-09-04 22:45:13 +0300

    Task 60 done

--- Task60/Program.cs
+++ Task60/Program.cs
@@ -9,3 +9,42 @@
 // 26(1,0,1) 55(1,1,1)
 
 
+int[,,] CreateMatrixRndInt(int row, int col, int we, int min, int max)
+{
+    int[,,] matrix = new int[row, col, we];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for(int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i,j, k] = rnd.Next(min, max+1);
+            }
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        // Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for(int k = 0; k < matrix.GetLength(2); k++)
+            {
+            Console.Write($"{matrix[i,j,k],4}({i},{j},{k})");
+            }
+
+        }
+        // Console.WriteLine("]");
+    }
+}
+
+
+int[,,] matrix = CreateMatrixRndInt(2, 2, 2, 10, 99);
+PrintMatrix(matrix);
