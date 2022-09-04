b94d8a746a5de9783043ecbe5c466a93cb7a1ae2
Masterov_IN <masterov_in@mail.ru>
2022-09-04 22:47:01 +0300

    Task 62 not done

--- Task62/Program.cs
+++ Task62/Program.cs
@@ -5,3 +5,48 @@
 // 11 16 15 06
 // 10 09 08 07
 
+int[,] CreateMatrixRndInt(int row, int col)
+{
+    int size = 4;
+    int count = 1;
+    row = size;
+    col = size;
+    int[,] matrix = new int[row, col];    
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] = count;
+                count++;
+                if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+                    j++;
+                else if (i < j && i + j >= matrix.GetLength(0) - 1)
+                    i++;
+                else if (i >= j && i + j > matrix.GetLength(1) - 1)
+                    j--;
+                else
+                    i--;        
+            }
+        }    
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} ");
+            else Console.Write($"{matrix[i, j],4} ");
+
+        }
+        Console.WriteLine("]");
+    }
+}
+int[,] matrix = CreateMatrixRndInt(4, 4);
+PrintMatrix(matrix);
